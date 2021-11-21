0770d412b43f6c635bcc4aa64380ab43dc0c004c
Sujal Singh <email.sujalsingh@gmail.com>
2021-11-21 08:29:05 +0530

    FIX: setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gflight-api",
+    name="gflight_api",
     version="0.0.1",
     author="Sujal Singh",
     author_email="email.sujalsingh@gmail.com",
